71b5798ead1936590a0b7d6e48cad9b92f3e9c87
hariseduardandrei <hariseduardandrei@gmail.com>
2016-09-29 16:29:40 +0300

    connection manager - heroku db

--- src/main/java/ro/jademy/persistance/ConnectionManager.java
+++ src/main/java/ro/jademy/persistance/ConnectionManager.java
@@ -9,7 +9,7 @@ public class ConnectionManager {
 	public static Connection getConnection() {
 		try {
 			Connection connection = DriverManager.getConnection(
-					"postgres://xvseorxagpkwwn:PTA8mq2JR2mBe1eR4IY6AhInua@ec2-54-243-203-141.compute-1.amazonaws.com:5432/db097ee5t7rs7r",
+					"jdbc:postgresql://ec2-54-243-203-141.compute-1.amazonaws.com:5432/db097ee5t7rs7r?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory",
 					"xvseorxagpkwwn", "PTA8mq2JR2mBe1eR4IY6AhInua");
 			return connection;
 		} catch (SQLException e) {
